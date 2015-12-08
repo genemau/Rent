763ebb8ee00d17bf7ca221c9e8e66dc0f2b42884
genemau <genemau@users.noreply.github.com>
2015-12-08 21:20:50 +1100

    Update scraper.py
    m

--- scraper.py
+++ scraper.py
@@ -1,24 +1,124 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+Skip to content
+Sign up Sign in
+This repository  
+Search
+Explore
+Features
+Enterprise
+Pricing
+ Watch 1  Star 0  Fork 0 genemau/Doma_r
+ Code  Issues 0  Pull requests 0  Pulse  Graphs
+Branch: master Find file Copy pathDoma_r/scraper.py
+6644e77  on Aug 10
+@genemau genemau Update scraper.py
+1 contributor
+RawBlameHistory     108 lines (79 sloc)  2.71 KB
+import scraperwiki
+from lxml import html
+import lxml
+from datetime import datetime
+import csv
+import urllib
+import re
+import string
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+extractedOn = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+dictReader = csv.DictReader(open('suburbs.csv', 'rb'))
+errorValue=''
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def getNextLink(root):
+
+    try:
+       return "http://www.realestate.com.au" + root.cssselect('li[class=nextLink] a[href]')[0].get('href') 
+    
+    except:
+       return ''      
+
+
+def getStartURL(suburb, postcode):
+    return "http://www.realestate.com.au/rent/with-2-bedrooms-in-"+ re.sub(" ","+",suburb) +"%2c+vic+" + postcode + "%3b/list-1?includeSurrounding=false"
+
+def parseHouse(houseEl, suburb):
+        #get the address
+        try:
+            h_address = houseEl.cssselect('a[class=name]')[0].text
+        except:
+            h_address = errorValue
+
+        #print 'address: ' + h_address
+
+        try:
+            features=houseEl.cssselect('dl[class^=rui-property-features] dd')
+
+            h_bedrooms = features[0].text
+            h_bathrooms = features[1].text
+            h_carparks = features[2].text
+
+
+        except:
+            h_bedrooms = errorValue
+            h_bathrooms = errorValue
+            h_carparks = errorValue
+
+
+        try:
+            h_price = houseEl.cssselect('p[class=priceText]')[0].text
+
+        except:
+            h_price = errorValue
+
+        try:
+            h_salesType = houseEl.cssselect('div[class="propertyStats"] p[class=type]')[0].text
+
+        except:
+            h_salesType = errorValue
+
+        # Save found data
+        scraperwiki.sqlite.save(unique_keys=['extracted_on','address'], data={
+            "extracted_on": extractedOn,
+            "suburb": suburb,
+	    "address": h_address,
+            "bedrooms": h_bedrooms,
+            "bathrooms": h_bathrooms,
+            "carparks": h_carparks,
+            "sales_type": h_salesType,
+            "price_text": h_price
+            })
+        #print h_address
+        #print h_bedrooms
+        #print h_bathrooms
+        #print h_price
+        #print h_salesType
+        #print '----------------------------------------------------'
+
+
+def parse(url, suburb):
+
+    html = scraperwiki.scrape(url)
+    root = lxml.html.fromstring(html)
+
+    #iterate through houses
+    for houseEl in root.cssselect('article[class^=resultBody]'):
+        
+        try:
+            parseHouse(houseEl, suburb)
+        except:
+            print 'Error: Cannot parse house!'
+
+
+    nextLink = getNextLink(root)
+
+    if nextLink == '':
+        print suburb + ' parsed'
+    else:
+        parse(nextLink, suburb)
+
+#parse(getStartURL('cheltenham', '3030'))
+
+for line in dictReader:
+    print 'Scraping ' + line["Suburb"]  	
+    parse(getStartURL(
+       suburb=line["Suburb"],
+       postcode=line["PostCode"]), line["Suburb"])
+Status API Training Shop Blog About Pricing
+© 2015 GitHub, Inc. Terms Privacy Security Contact Help
